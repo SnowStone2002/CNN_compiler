a5285cbd40b5ff0274eb4da7577c017960f9525a
He Wang <wanghe020608@gmail.com>
2024-05-05 16:16:03 +0800

    initial compiler for graduate thesis

---
+++ Makefile
@@ -0,0 +1,29 @@
+# Makefile
+
+# Compiler and flags
+CC=gcc
+CFLAGS=-fdiagnostics-color=always -g -Wall
+
+# Source files
+SOURCES=compiler.c hw_config.c 
+
+# Object files
+OBJECTS=$(SOURCES:.c=.o)
+
+# Target executable
+TARGET=compiler
+
+# Default target
+all: $(TARGET)
+
+# Linking the target executable from the object files
+$(TARGET): $(OBJECTS)
+	$(CC) $(CFLAGS) $(OBJECTS) -o $@
+
+# # Compiling source files into object files
+%.o: %.c
+	$(CC) $(CFLAGS) -c $< -o $@
+
+# Cleaning up the compilation products
+clean:
+	rm -f $(OBJECTS) $(TARGET)
---
+++ compiler.c
@@ -0,0 +1,154 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include "hw_config.h"
+
+#define min(a,b) ((a) < (b) ? (a) : (b))
+#define WRITE_INST 0
+
+int bus_width, al, pc, scr, is_depth, os_depth, freq;
+
+hwc hw;
+Config config;
+
+int load_is_narrow_bus(int num_rows, int input_map_position)
+
+void conv2d(
+    int input_size,
+    int input_channel,
+    int output_channel,
+    int kernel_size,
+    int stride,
+    int padding,
+    int input_addr_base,
+    int weight_addr_base,
+    int output_addr_base
+);
+
+int main(){
+    bus_width = 128;
+    al = 64;
+    pc = 8;
+    scr = 8;
+    is_depth = 1024;
+    os_depth = 1024;
+    freq = 500;
+    InitConfig(&config, bus_width, al, pc, scr, is_depth, os_depth, freq);
+    Inithwc(&hw, config);
+    conv2d(112,64,128,3,1,2,0,0,0);
+
+    return 0;
+}
+
+int load_is_narrow_bus(int num_rows, int input_map_position) {
+    for (int i_rows = 0; i_rows < num_rows; ++i_rows) {
+        input_map_position += (config.BUS_WIDTH / config.DATA_WIDTH) * (acc0.InputSRAMWidth / acc0.BusWidth - 1);
+        for (int j_reg = acc0.InputSRAMWidth / acc0.BusWidth - 1; j_reg >= 0; --j_reg) {
+            if (j_reg != 0) {
+                instructionCount.Linp++;
+                if (WRITE_INST){
+                    if (VERIFY) {
+                        sprintf(item, "Linp\t <pos> %d\t <is_addr> %d\t <input_map> %d\n", j_reg, i_rows, input_map_position);
+                        PushInstStack(&inst_stack, item, 0, 0);
+                    } else {
+                        sprintf(item, "Linp\t <pos> %d\n", j_reg);
+                        PushInstStack(&inst_stack, item, 0, 0);
+                    }
+                }
+            } else {
+                instructionCount.Lin++;
+                if (WRITE_INST){
+                    if (VERIFY) {
+                        sprintf(item, "Lin\t\t <pos> %d\t <is_addr> %d\t <input_map> %d\n", j_reg, i_rows, input_map_position);
+                        PushInstStack(&inst_stack, item, 0, 0);
+                    } else {
+                        sprintf(item, "Lin\t\t <pos> %d\t <is_addr> %d\n", j_reg, i_rows);
+                        PushInstStack(&inst_stack, item, 0, 0);
+                    }
+                }
+            }
+            input_map_position -= (config.BUS_WIDTH / config.DATA_WIDTH);
+            // 注意: 每个channel的最后一行可能需要特殊处理
+        }
+        input_map_position += (acc0.InputSRAMWidth / config.DATA_WIDTH) + (config.BUS_WIDTH / config.DATA_WIDTH);
+    }
+    return input_map_position;
+}
+
+void conv2d(
+    int input_size,
+    int input_channel,
+    int output_channel,
+    int kernel_size,
+    int stride,
+    int padding,
+    int input_addr_base,
+    int weight_addr_base,
+    int output_addr_base
+){
+    //basic calculation
+    int output_size = (input_size + 2 * padding - kernel_size) / stride + 1;
+
+    int acc_times_h = kernel_size;
+    int acc_times_w = kernel_size;
+    int acc_times_c = ceil((float)input_channel / hw.AL);
+
+    int acc_times = acc_times_c * acc_times_h * acc_times_w;
+    int para_times = ceil((float)output_channel / hw.PC);
+
+    int input_colomns_per_IS_load = hw.InputSRAMDepth / acc_times_c;
+    if (input_colomns_per_IS_load > input_size * input_size)
+        input_colomns_per_IS_load = input_size * input_size;
+    int IS_load_times_per_conv = (int)ceil((float)input_size * input_size / input_colomns_per_IS_load);
+
+    int* IS_load_columns = (int*)malloc(IS_load_times_per_conv * sizeof(int));
+
+    for (int i = 0; i < IS_load_times_per_conv; ++i) {
+        IS_load_columns[i] = input_colomns_per_IS_load;  // 默认每次载入处理最大列数
+    }
+    // 如果最后一次载入不能满载，则调整最后一次载入的列数
+    if ((input_size * input_size) % input_colomns_per_IS_load != 0) {
+        IS_load_columns[IS_load_times_per_conv - 1] = (input_size * input_size) % input_colomns_per_IS_load;
+    }
+
+    int weight_update_times_per_conv = (int)ceil((float)acc_times / config.SCR) * para_times;
+    int* weight_update_ls = (int*)malloc(weight_update_times_per_conv * sizeof(int));
+
+    // 初始化权重更新次数数组
+    int weight_updates_index = 0;
+    for (int p = 0; p < para_times; p++) {
+        int times_per_para = (int)ceil((float)acc_times / hw.SCR);
+        for (int i = 0; i < times_per_para; i++) {
+            if (i < times_per_para - 1) {
+                // 通常情况，使用满额SCR值
+                weight_update_ls[weight_updates_index] = hw.SCR;
+                weight_updates_index++;
+            } else {
+                // 每个参数周期的最后一个元素可能不是满额
+                int last_amount = acc_times % hw.SCR;
+                if (last_amount == 0) {  // 如果acc_times是config.SCR的倍数，则为满额
+                    weight_update_ls[weight_updates_index] = hw.SCR;
+                    weight_updates_index++;
+                } else {  // 否则，使用余数作为最后一次更新的大小
+                    weight_update_ls[weight_updates_index] = last_amount;
+                    weight_updates_index++;
+                }
+            }
+        }
+    }
+
+    // 验证更新列表是否正确
+    // printf("Weight update times per conv:\n");
+    // for (int i = 0; i < weight_update_times_per_conv; i++) {
+    //     printf("%d ", weight_update_ls[i]);
+    // }
+    // printf("\n");
+
+
+    // 释放内存
+    free(IS_load_columns);
+    free(weight_update_ls);
+    //return 0;
+};
